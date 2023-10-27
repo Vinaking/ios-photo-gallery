e1397c7c2731ab90e1ee42fa457f6f27d5eb88e7
TungH <tungh@ichiisoft.com>
2023-10-27 11:34:38 +0700

    Initial Commit

---
+++ PhotoGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by Tùng on 27/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
